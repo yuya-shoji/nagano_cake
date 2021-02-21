3249a126d7f5320472f9cbd2b374c6d4e013c13a
EC2 Default User <ec2-user@ip-172-31-58-138.ec2.internal>
2021-02-21 18:36:17 +0000

    Minor correction

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -11,14 +11,21 @@ class AddressesController < ApplicationController
 
   def create
     @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
     @address.save
     redirect_to addresses_path
   end
 
   def update
+    address = Address.find(params[:id])
+    address.update(address_params)
+    redirect_to addresses_path
   end
 
   def destroy
+    address = Address.find(params[:id])
+    address.destroy
+    redirect_to addresses_path
   end
 
 private
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,5 +1,4 @@
 class CartItem < ApplicationRecord
     belongs_to :item
     belongs_to :customer
-    attachment :image
 end
